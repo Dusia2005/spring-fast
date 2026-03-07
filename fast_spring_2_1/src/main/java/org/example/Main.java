aeb66f0ce3dbf964399f265a9ee42ccea0a84b2a
Dasha Leshok <116278334+Dusia2005@users.noreply.github.com>
2026-03-07 22:27:18 +0300

    add materials and project

---
+++ fast_spring_2_1/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext();
+
+        Person Daria = new Person();
+
+
+    }
+}
---
+++ fast_spring_2_1/src/main/java/org/example/Person.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age <= 0 || age >= 120) {
+            System.out.println("Такого не может быть О_о");
+        } else {
+            this.age = age;
+        }
+    }
+}
